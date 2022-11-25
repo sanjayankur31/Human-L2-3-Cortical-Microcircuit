b496ee994d00c0a3a731df21a869ab0ac765dedf
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-11-25 14:26:01 +0000

    chore: add options to script

--- NeuroML2/convert_cells_to_neuroml.sh
+++ NeuroML2/convert_cells_to_neuroml.sh
@@ -25,5 +25,51 @@ function postprocess () {
     python postprocess_cells.py
 }
 
-convert_morphologies
-postprocess
+function clean() {
+    echo "Removing: *.cell.nml *.hoc *.mod LEMS* *.dat *.nrn.py x86_64 iv*nml"
+    rm *.cell.nml *.hoc iv*nml
+    rm -f *.mod LEMS* *.dat *nrn.py
+    rm -rf x86_64
+}
+
+function setup () {
+    libnrnmechdir="$(dirname $(find . -maxdepth 2 -name "libnrnmech*" ))"
+    echo "Removing ${libnrnmechdir}"
+    rm -rf "${libnrnmechdir}"
+    echo "Compiling mods"
+    nrnivmodl mod
+}
+
+# Do everything without any arguments
+if [ $# -lt 1 ]
+then
+    setup
+    convert_morphologies
+    postprocess
+    exit 0
+fi
+
+# parse options
+while getopts "csm" OPTION
+do
+    case $OPTION in
+        c)
+            clean
+            exit 0
+            ;;
+        s)
+            setup
+            exit 0
+            ;;
+        m)
+            clean
+            setup
+            convert_morphologies
+            exit 0
+            ;;
+        ?)
+            echo "Error: unrecognised option"
+            exit 1
+            ;;
+    esac
+done
