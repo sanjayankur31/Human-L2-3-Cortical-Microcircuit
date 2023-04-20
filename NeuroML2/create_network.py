0a23f26eea081830632eececb1764cd69212bf74
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-04-20 18:05:47 +0100

    wip: init network creation script

---
+++ NeuroML2/create_network.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Parse population and connectivity information exported from circuit.py to
+create a NeuroML representation of the network.
+
+File: create_network.py
+
+Copyright 2023 Ankur Sinha
+"""
