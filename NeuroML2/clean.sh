dd5cea166a635273ad5c0fd57a8d99adb0e13c41
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-09-15 12:31:28 +0100

    chore: create folder in tmpdir to move files to
    otherwise /tmp gets littered with lots of files and one can't really
    figure out which files were generated when
    
    [skipci]

--- NeuroML2/clean.sh
+++ NeuroML2/clean.sh
@@ -2,4 +2,5 @@
 # Script to clean out generated files
 set -x
 rm -rf x86_64 arm64
-mv *.txt *hoc *.mod *dat *.c *.so *nrn.py *netpyne.py *_eden.py /tmp
+atmpdir=$(mktemp -d --suffix _nml)
+mv *.txt *hoc *.mod *dat *.c *.so *nrn.py *netpyne.py *_eden.py ${atmpdir}
