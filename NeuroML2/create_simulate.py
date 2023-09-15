fa728fd5477bcb6518816f14b35e44b6c31d6e71
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-09-15 21:57:28 +0100

    chore: rename script
    It creates and simulates.

--- NeuroML2/create_simulate.py
+++ NeuroML2/create_simulate.py
@@ -3,7 +3,7 @@
 Parse population and connectivity information exported from circuit.py to
 create a NeuroML representation of the network.
 
-File: create_network.py
+File: create_simulate.py
 
 Copyright 2023 Ankur Sinha
 """
