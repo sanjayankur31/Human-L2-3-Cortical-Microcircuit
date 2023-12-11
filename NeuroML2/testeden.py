01392b5211e248ec1ea34055415c83a26d8f989b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-12-11 17:04:16 +0000

    chore: add eden test scripts

---
+++ NeuroML2/testeden.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Enter one line description here.
+
+File:
+
+Copyright 2023 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+from pyneuroml import pynml
+
+
+pynml.run_lems_with_eden("./LEMS_HL23SST_sim.xml")
---
+++ NeuroML2/testedennative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Enter one line description here.
+
+File:
+
+Copyright 2023 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+import eden_simulator
+
+eden_simulator.run_sim.runEden("./LEMS_HL23SST_sim.xml")
+
