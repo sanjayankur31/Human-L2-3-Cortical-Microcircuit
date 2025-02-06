23384deeec29c73c96e094acda4f1a4fd2819d3c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-02-06 17:34:38 +0000

    feat: generalise script to take LEMS sim file as argument

--- NeuroML2/nml_v2spikes.py
+++ NeuroML2/nml_v2spikes.py
@@ -8,26 +8,73 @@ Copyright 2025 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
+import sys
+
 import elephant
 import quantities
+from lems.model.model import Model
 from neo.io import AsciiSignalIO
+from pyneuroml.io import read_neuroml2_file
 from pyneuroml.plot.PlotSpikes import plot_spikes
+from pyneuroml.utils.units import get_value_in_si
 
-# TODO
-# Should be able to get number of cells from the NeuroML network description
-# instead of hard coding it.
-# The order of recordings will have to be picked from the LEMS file, though
-cells = {"PYR": 160, "SST": 10, "PV": 14, "VIP": 16}
-# 160 PYR, 10 SST, 14 PV, 16 VIP
-data_filename = "HL23Net_0.2.v.dat"
+if len(sys.argv) < 2:
+    print("Script takes one argument: name of the LEMS simulation file")
+    sys.exit(1)
+
+
+print(f"Processing simulation file: {sys.argv[1]}")
+
+model = Model(include_includes=True, fail_on_missing_includes=False)
+model.import_from_file(sys.argv[1])
+
+# Note: could have also just parsed the Components here too
+network_file = ""
+for inc in model.included_files:
+    if inc.endswith(".net.nml"):
+        network_file = inc
+
+network_doc = read_neuroml2_file(network_file)
+populations = network_doc.networks[0].populations
+
+cells = {}
+total_cells = 0
+for pop in populations:
+    cell_type = pop.id.replace("HL23", "").replace("_pop", "")
+    pop_size = len(pop.instances)
+    cells[cell_type] = pop_size
+    total_cells += pop_size
+
+
+sampling_rate = 1 * quantities.Hz
+data_filename = ""
+
+simulation_components = model.get_component_list("Sim")
+for com_id, comp in simulation_components.items():
+    if comp.type == "Simulation":
+        params = comp.parameters
+        sampling_rate = (1 / get_value_in_si(params["step"])) * quantities.Hz
+
+        for ch in comp.children:
+            if ch.type == "OutputFile":
+                params = ch.parameters
+                data_filename = params["fileName"]
+
+print(f"Cells in model are: {cells}")
+print(f"Samping rate is: {sampling_rate}")
+print(f"Data file is: {data_filename}")
+
+assert len(network_file) > 0
+assert len(data_filename) > 0
+
 # 200 colums, but skip the 0th time column, and the spurious last column
-cols = list(range(1, 201))
+cols = list(range(1, total_cells + 1))
 
 # sampling rate is 1/dt
 memb_pot_data = AsciiSignalIO(
-    "HL23Net_0.2.v.dat",
+    data_filename,
     timecolumn=None,
-    sampling_rate=40000.0 * quantities.Hz,
+    sampling_rate=sampling_rate,
     units=quantities.volt,
     time_units=quantities.second,
     t_start=0 * quantities.second,
